b4652f25d72394c92291bcc7385365fc9553955f
t-sakurai816 <t.devsaku@gmail.com>
2021-08-24 19:59:38 +0900

    FIX: add package

--- install_check.sh
+++ install_check.sh
@@ -2,7 +2,7 @@
 function check_git() {
   if ! [ -x "$(command -v git)" ]; then
     echo 'Error: git is not installed.' >&2
-    # apk add --no-cache git
+    apk add --no-cache git
   else
     echo 'git is installed.'
   fi
@@ -12,7 +12,7 @@ function check_git() {
 function check_python3() {
   if ! [ -x "$(command -v python3)" ]; then
     echo 'Error: python3 is not installed.' >&2
-    # apk add --no-cache python3
+    apk add --no-cache python3
   else
     echo 'python3 is installed.'
   fi

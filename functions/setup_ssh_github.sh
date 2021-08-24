72dc86a92c0a0dd5ba8413a04fd7077b92380e49
t-sakurai816 <t.devsaku@gmail.com>
2021-08-24 22:48:55 +0900

    UPDATE: add config

--- functions/setup_ssh_github.sh
+++ functions/setup_ssh_github.sh
@@ -2,15 +2,30 @@ function generate_github_rsa() {
   echo "Generating RSA key pair..."
   ssh-keygen -t rsa -N "" -f ~/.ssh/github_rsa
   echo "Done."
-  cat ~/.ssh/github_rsa.pub > /dev/clipboard | cat /dev/clipboard
+  cat ~/.ssh/github_rsa.pub > /dev/clipboard
   echo "Public key copied to clipboard."
+}
+
+function add_ssh_github_config() {
+  echo "Adding SSH config..."
+  cat << EOF > ~/.ssh/config
+Host github github.com
+  HostName github.com
+  IdentityFile ~/.ssh/github_rsa
+  User git
+EOF
 }
 
 function setup_ssh_github() {
   if [ ! -e ~/.ssh/github_rsa ]; then
     generate_github_rsa
+    if grep -q "github.com" ~/.ssh/config; then
+      echo "SSH config already exists."
+    else
+      add_ssh_github_config
+    fi
     echo "###################################"
-    echo "You go the https://github.com/settings/keys ."
+    echo "You go the https://github.com/settings/keys."
     echo "Run the following command to verify"
     echo "ssh -T git@github.com"
     echo "###################################"

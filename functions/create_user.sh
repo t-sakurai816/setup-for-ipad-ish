09d22d24050f7f5628efbf7bb869118a80d831f9
t-sakurai816 <t.devsaku@gmail.com>
2021-08-25 23:56:58 +0900

    DELETE: check

--- functions/create_user.sh
+++ functions/create_user.sh
@@ -1,14 +1,6 @@
 function create_user() {
   user=$1
   group=wheel
-  if [ -z "$user" ]; then
-    echo "Error: No user specified"
-    exit 1
-  fi
-  if [ -z "$group" ]; then
-    echo "Error: No group specified"
-    exit 1
-  fi
   if [ -e "/home/$user" ]; then
     echo "Error: User $user already exists"
     exit 1

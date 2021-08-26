d652680d2244b668cb936843b78ca26c77e5276f
t-sakurai816 <t.devsaku@gmail.com>
2021-08-26 14:00:13 +0900

    FIX

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/ein/ash
 
 source ./functions/install_check.sh
+source ./functions/setup_ssh_github.sh
 source ./functions/create_user.sh
 
 echo -e "\nInstall check"
@@ -23,51 +24,9 @@ echo -e "\n\n"
 echo -e "Creating user"
 create_user $1
 
-su $1 <<EOF
-whoami
+sudo -u $1 whoami
 
 echo -e "Setup SSH for GitHub\n"
 
-function generate_github_rsa() {
-  echo "Generating RSA key pair..."
-  ssh-keygen -t rsa -N "" -f ~/.ssh/github_rsa
-  echo "Done."
-  cat ~/.ssh/github_rsa.pub > /dev/clipboard
-  echo "Public key copied to clipboard."
-}
+sudo -u $1 setup_ssh_github
 
-function add_ssh_github_config() {
-  echo "Adding SSH config..."
-  cat << EOF > ~/.ssh/config
-Host github github.com
-  HostName github.com
-  IdentityFile ~/.ssh/github_rsa
-  User git
-\EOF
-}
-
-function setup_ssh_github() {
-  if [ ! -e ~/.ssh/github_rsa ]; then
-    generate_github_rsa
-    if grep -q "github.com" ~/.ssh/config; then
-      echo "SSH config already exists."
-    else
-      add_ssh_github_config
-    fi
-    echo "###################################"
-    echo "You go the https://github.com/settings/keys."
-    echo "Run the following command to verify"
-    echo "ssh -T git@github.com"
-    echo "###################################"
-  else
-    echo "###################################"
-    echo "GitHub RSA key pair already exists."
-    echo "Maybe you're setting up SSH for GitHub!"
-    echo "Run the following command to confirm. If it fails, go to the https://github.com/settings/keys. and check it out"
-    echo "ssh -T git@github.com"
-    echo "###################################"
-  fi
-}
-
-setup_ssh_github
-EOF
